50e9baa1a560ed4d752ed6ca771a00ff2afe2c96
SSV96 <sathyaswaroopvandavasi@gmail.com>
2024-11-13 16:16:50 +0530

    Eslint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Home = () => {
+  return (
+    <h1 className="text-3xl  font-black text-white ">
+      Welcome to the world of next js
+    </h1>
+  );
 };
 
-export default page;
+export default Home;
