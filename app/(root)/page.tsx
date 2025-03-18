0199021ace3cab5e260d01422c7967ad9a3a4fe7
SSV96 <sathyaswaroopvandavasi@gmail.com>
2025-03-18 09:37:19 +0530

    Setup Auth Layouts, Social Auth Form,Next Auth, OAuth with Google and GitHub Provider

---
+++ app/(root)/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of next js</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">LogOut</Button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
+// lesson 33
---
+++ constants/routes.ts
@@ -0,0 +1,9 @@
+const ROUTES = {
+  HOME: "/",
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up",
+  PROFILE: "/profile",
+  NOT_FOUND: "*",
+};
+
+export default ROUTES;
